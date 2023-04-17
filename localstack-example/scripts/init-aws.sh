2e19607a3ceb7a2b28d6879ce1ff39a982654faf
valdemarjuniorr <valdemarjuniorr@gmail.com>
2023-04-17 14:58:38 -0300

    making init localstack script works

--- localstack-example/scripts/init-aws.sh
+++ localstack-example/scripts/init-aws.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 awslocal sns create-topic --name notification-topic
 awslocal sqs create-queue --queue-name notification-queue
 awslocal --endpoint-url=http://localhost:4566 sns subscribe --topic-arn arn:aws:sns:us-east-1:000000000000:notification-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:notification-queue
