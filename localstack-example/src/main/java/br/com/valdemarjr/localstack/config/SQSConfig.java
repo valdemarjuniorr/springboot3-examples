c1eee0bb9605dcee334e68f5cd5eebda084a4c51
valdemarjuniorr <valdemarjuniorr@gmail.com>
2023-04-17 15:32:01 -0300

    removing commented code

--- localstack-example/src/main/java/br/com/valdemarjr/localstack/config/SQSConfig.java
+++ localstack-example/src/main/java/br/com/valdemarjr/localstack/config/SQSConfig.java
@@ -2,14 +2,11 @@ package br.com.valdemarjr.localstack.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSAsyncClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
@@ -42,8 +39,8 @@ public class SQSConfig {
   }
 
   @Bean
-  public QueueMessagingTemplate queueMessagingTemplate(AmazonSQSAsync amazonSQSAsync) {
-    return new QueueMessagingTemplate(amazonSQSAsync);
+  public QueueMessagingTemplate queueMessagingTemplate() {
+    return new QueueMessagingTemplate(amazonSQSAsync());
   }
 
   @Bean
