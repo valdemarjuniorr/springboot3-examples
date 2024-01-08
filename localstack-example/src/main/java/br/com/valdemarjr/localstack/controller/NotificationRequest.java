6ff2dcaff2aac9a67851d63e5b3bd92b55d3ce7d
Valdemar Jr <valdemar.barros@livelo.com.br>
2024-01-08 14:30:06 -0300

    change request to record

--- localstack-example/src/main/java/br/com/valdemarjr/localstack/controller/NotificationRequest.java
+++ localstack-example/src/main/java/br/com/valdemarjr/localstack/controller/NotificationRequest.java
@@ -1,15 +1,8 @@
 package br.com.valdemarjr.localstack.controller;
 
 import br.com.valdemarjr.localstack.domain.NotificationMessage;
-import lombok.Getter;
-import lombok.Setter;
 
-@Setter
-@Getter
-class NotificationRequest {
-  private String from;
-  private String to;
-  private String content;
+record NotificationRequest(String from, String to, String content) {
 
   public NotificationMessage toDomain() {
     return new NotificationMessage(this.from, this.to, this.content);
