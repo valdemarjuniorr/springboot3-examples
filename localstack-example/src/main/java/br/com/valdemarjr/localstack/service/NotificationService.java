a2809c136a6733f2d718117a2f3d1601d8d89e9d
Valdemar Jr <valdemar.barros@livelo.com.br>
2024-02-19 16:52:46 -0300

    rollback commits by accident

--- localstack-example/src/main/java/br/com/valdemarjr/localstack/service/NotificationService.java
+++ localstack-example/src/main/java/br/com/valdemarjr/localstack/service/NotificationService.java
@@ -18,7 +18,7 @@ public class NotificationService {
   private final QueueMessagingTemplate messagingTemplate;
 
   public void notifyTopic(NotificationMessage message) {
-    log.info("Notifying topic {}", config.getTopic˜());
+    log.info("Notifying topic {}", config.getTopic());
     notificationTemplate.sendNotification(config.getTopic(), message, "notification");
   }
 
