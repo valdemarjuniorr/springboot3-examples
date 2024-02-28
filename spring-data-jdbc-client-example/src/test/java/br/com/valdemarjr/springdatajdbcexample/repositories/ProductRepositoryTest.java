6e277c34e7718cdf7fe8dc3f189bd14e059a6d3a
Valdemar Jr <valdemar.barros@livelo.com.br>
2024-02-28 09:40:30 -0300

    added unit tests

--- spring-data-jdbc-client-example/src/test/java/br/com/valdemarjr/springdatajdbcexample/repositories/ProductRepositoryTest.java
+++ spring-data-jdbc-client-example/src/test/java/br/com/valdemarjr/springdatajdbcexample/repositories/ProductRepositoryTest.java
@@ -1,17 +1,19 @@
 package br.com.valdemarjr.springdatajdbcexample.repositories;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import br.com.valdemarjr.springdatajdbcexample.domain.Product;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.jdbc.core.simple.JdbcClient;
-
-import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 class ProductRepositoryTest {
@@ -36,5 +38,63 @@ class ProductRepositoryTest {
     var product = productRepository.findBy(ID);
 
     assertThat(product).isNotNull().hasValue(productMock);
+  }
+
+  @Test
+  void findAll() {
+    var productMock = new Product(ID, DESCRIPTION);
+    var statementMock = mock(JdbcClient.StatementSpec.class);
+    var mappedQuery = mock(JdbcClient.MappedQuerySpec.class);
+
+    when(jdbcClient.sql(anyString())).thenReturn(statementMock);
+    when(statementMock.query(Product.class)).thenReturn(mappedQuery);
+    when(mappedQuery.list()).thenReturn(List.of(productMock));
+
+    var products = productRepository.findAll();
+
+    assertThat(products).isNotNull().isNotEmpty().hasSize(1).contains(productMock);
+  }
+
+  @Test
+  void create() {
+    var productMock = new Product(ID, DESCRIPTION);
+    var statementMock = mock(JdbcClient.StatementSpec.class);
+
+    when(jdbcClient.sql(anyString())).thenReturn(statementMock);
+    when(statementMock.param("id", ID)).thenReturn(statementMock);
+    when(statementMock.param("description", DESCRIPTION)).thenReturn(statementMock);
+    when(statementMock.update()).thenReturn(1);
+
+    var result = productRepository.create(productMock);
+
+    assertThat(result).isNotNull().isEqualTo(1);
+  }
+
+  @Test
+  void delete() {
+    var statementMock = mock(JdbcClient.StatementSpec.class);
+
+    when(jdbcClient.sql(anyString())).thenReturn(statementMock);
+    when(statementMock.param("id", ID)).thenReturn(statementMock);
+    when(statementMock.update()).thenReturn(1);
+
+    var result = productRepository.delete(ID);
+
+    assertThat(result).isNotNull().isEqualTo(1);
+  }
+
+  @Test
+  void update() {
+    var productMock = new Product(ID, DESCRIPTION);
+    var statementMock = mock(JdbcClient.StatementSpec.class);
+
+    when(jdbcClient.sql(anyString())).thenReturn(statementMock);
+    when(statementMock.param("description", DESCRIPTION)).thenReturn(statementMock);
+    when(statementMock.param("id", ID)).thenReturn(statementMock);
+    when(statementMock.update()).thenReturn(1);
+
+    var result = productRepository.update(productMock);
+
+    assertThat(result).isNotNull().isEqualTo(1);
   }
 }
