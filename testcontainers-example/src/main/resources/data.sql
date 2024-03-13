27c9f984ffada2601c1d8d7e5c46cf89d965edd9
Valdemar Jr <valdemar.barros@livelo.com.br>
2024-03-13 13:51:13 -0300

    delete row if data.sql runs again on restart

--- testcontainers-example/src/main/resources/data.sql
+++ testcontainers-example/src/main/resources/data.sql
@@ -1,3 +1,5 @@
+DELETE FROM PRODUCT;
+
 INSERT INTO PRODUCT(id, name) VALUES(1, 'Product one');
 INSERT INTO PRODUCT(id, name) VALUES(2, 'Product two');
 INSERT INTO PRODUCT(id, name) VALUES(3, 'Product three');
