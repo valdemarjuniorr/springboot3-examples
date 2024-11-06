5a5737fb9e343885d5628853e1e28af353c69fbe
Valdemar Júnior <valdemarjuniorr@gmail.com>
2024-11-05 23:25:40 -0300

    Delete springboot-read-replicas-example directory
    Remove directory commited by accident

--- springboot-read-replicas-example/src/main/java/br/com/valdemarjr/springboot_read_replicas_example/repository/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.valdemarjr.springboot_read_replicas_example.repository;
-
-import br.com.valdemarjr.springboot_read_replicas_example.domain.Product;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface ProductRepository extends ListCrudRepository<Product, Long> {}
--- springboot-read-replicas-example/src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS product
-(
-    id    serial PRIMARY KEY,
-    name  VARCHAR(255)   NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
-)
