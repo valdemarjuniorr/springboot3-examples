f5988810ea3c3cea8eaff707e781b849d058e7df
Valdemar Jr <valdemar.barros@livelo.com.br>
2025-01-30 11:19:09 -0300

    Update Spring boot, spring ai and java version

--- spring-ai-ollama-example/src/main/java/br/com/valdemarjr/springaiexample/client/ChatAiClient.java
+++ spring-ai-ollama-example/src/main/java/br/com/valdemarjr/springaiexample/client/ChatAiClient.java
@@ -2,28 +2,27 @@ package br.com.valdemarjr.springaiexample.client;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ai.chat.ChatClient;
-import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ChatAiClient {
 
   private static final Logger log = LoggerFactory.getLogger(ChatAiClient.class);
-
   private final ChatClient chatClient;
 
-  public ChatAiClient(ChatClient chatClient) {
-    this.chatClient = chatClient;
+  public ChatAiClient(ChatClient.Builder builder) {
+    this.chatClient = builder.build();
   }
 
   public String chat(String question) {
     log.info("chat - question: {}", question);
 
-    var aiResponse = this.chatClient.call(new Prompt(question));
-    var response = aiResponse.getResult().getOutput().getContent();
+    var aiResponse = this.chatClient.prompt(question);
+    var response = aiResponse.call().chatResponse().getResult();
 
 	log.info("chat - question answered: {}", response);
-    return response;
+
+    return response.toString();
   }
 }
