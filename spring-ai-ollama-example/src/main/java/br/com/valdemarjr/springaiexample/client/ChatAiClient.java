319c2760527e2d56efe90bed629218287cdbc1a1
Valdemar Jr <valdemar.barros@livelo.com.br>
2025-01-31 11:11:54 -0300

    update ChatAiClient

--- spring-ai-ollama-example/src/main/java/br/com/valdemarjr/springaiexample/client/ChatAiClient.java
+++ spring-ai-ollama-example/src/main/java/br/com/valdemarjr/springaiexample/client/ChatAiClient.java
@@ -3,6 +3,7 @@ package br.com.valdemarjr.springaiexample.client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,18 +12,17 @@ public class ChatAiClient {
   private static final Logger log = LoggerFactory.getLogger(ChatAiClient.class);
   private final ChatClient chatClient;
 
-  public ChatAiClient(ChatClient.Builder builder) {
-    this.chatClient = builder.build();
+  public ChatAiClient(OllamaChatModel model) {
+    this.chatClient = ChatClient.create(model);
   }
 
   public String chat(String question) {
     log.info("chat - question: {}", question);
 
-    var aiResponse = this.chatClient.prompt(question);
-    var response = aiResponse.call().chatResponse().getResult();
+    var aiResponse = this.chatClient.prompt(question).call().content();
 
-	log.info("chat - question answered: {}", response);
+	log.info("chat - question answered: {}", aiResponse);
 
-    return response.toString();
+    return aiResponse;
   }
 }
